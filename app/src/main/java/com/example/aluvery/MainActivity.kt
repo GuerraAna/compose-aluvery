cdbfcaf028a644078147861de7ef3269fed30da4
GuerraAna <ana.guerrads@hotmail.com>
2023-12-17 21:45:12 -0300

    create product card item

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aluvery
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -9,80 +10,115 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.TopCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aluvery.ui.theme.AluveryTheme
+import com.example.aluvery.ui.theme.Purple500
+import com.example.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContent {
-			AluveryTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(
-						modifier = Modifier.fillMaxSize(),
-						color = MaterialTheme.colorScheme.background
-				) {
-					AuthorMessage(authorInfo = Message(R.drawable.ic_launcher_foreground,"Ana", "É sobre isso, tudo bem"))
-				}
-			}
-		}
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AluveryTheme {
+                Surface {
+                    ProductItem()
+                }
+            }
+        }
+    }
 }
 
-data class Message(val image: Int, val author: String, val message: String)
-
 @Composable
-fun AuthorMessage(authorInfo: Message) {
-	Row {
-		Image(
-				painter = painterResource(id = authorInfo.image),
-				contentDescription = "Android",
-				colorFilter = ColorFilter.tint(Color.Blue),
-				modifier = Modifier
-					.size(40.dp)
-					.clip(CircleShape)
-					.background(Color(R.color.black))
-					.align(CenterVertically)
-		)
+fun ProductItem() {
+    Surface(
+            modifier = Modifier.padding(8.dp),
+            shape = RoundedCornerShape(size = 15.dp),
+            tonalElevation = 16.dp,
+            shadowElevation = 4.dp
+    ) {
+        Column(
+                modifier = Modifier
+                        .heightIn(250.dp, 300.dp)
+                        .width(200.dp)
+        ) {
+            val imageSize = 100.dp
 
-		Spacer(modifier = Modifier.width(8.dp))
+            Box(
+                    modifier = Modifier
+                            .height(imageSize)
+                            .fillMaxWidth()
+                            .background(
+                                    brush = Brush.horizontalGradient(
+                                            colors = listOf(Purple500, Teal200)
+                                    )
+                            )
+            ) {
+                Image(
+                        painter = painterResource(R.drawable.ic_launcher_background),
+                        contentDescription = null,
+                        modifier = Modifier
+                                .size(size = imageSize)
+                                .offset(y = imageSize/2)
+                                .clip(shape = CircleShape)
+                                .align(alignment = TopCenter)
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize/2))
 
-		Column {
-			Text(text = authorInfo.author)
-			Text(text = authorInfo.message)
-		}
-	}
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                        text = LoremIpsum(50).values.first(),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight(700),
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                        text = "R$14,001",
+                        modifier = Modifier.padding(top = 8.dp),
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight(400)
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "product_item", showSystemUi = false)
 @Composable
-fun AuthorMessagePreview() {
-	AluveryTheme {
-		Row {
-			AuthorMessage(
-					authorInfo = Message(
-							image = R.drawable.ic_launcher_foreground,
-							author = "Ana",
-							message = "Conheça e domine todas as técnicas, mas na hora de lidar com uma alma humana, seja apenas outra alma humana"))
-		}
-	}
+fun ProductItemPreview() {
+    ProductItem()
 }
