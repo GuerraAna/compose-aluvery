6568d1a0f722fc0da4c41b69151ab006b8aabb44
GuerraAna <ana.guerrads@hotmail.com>
2024-01-25 18:40:49 -0300

    use lazy row

--- app/src/main/java/com/example/aluvery/ui/components/section/ProductsSection.kt
+++ app/src/main/java/com/example/aluvery/ui/components/section/ProductsSection.kt
@@ -1,11 +1,11 @@
 package com.example.aluvery.ui.components.section
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,16 +35,15 @@ fun ProductSection(
             )
                 },
         content = {
-            Row(
+            LazyRow(
                 modifier = Modifier
                     .padding(top = 8.dp, bottom = 16.dp)
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .horizontalScroll(rememberScrollState()),
-                horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp)
             ) {
-                for (product in products) {
-                    ProductItem(product = product)
+                items(products) { p ->
+                    ProductItem(product = p)
                 }
             }
                    },
