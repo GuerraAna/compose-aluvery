56dfd253048e350fca72ebb4a43cfb27e9da03d2
GuerraAna <ana.guerrads@hotmail.com>
2024-02-15 15:22:57 -0300

    feature: implement ProductFormActivity screen and do some theme refactors WIP

--- app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
@@ -1,13 +1,20 @@
 package com.example.aluvery.ui.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
@@ -18,11 +25,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.aluvery.R
+import com.example.aluvery.model.Product
 import com.example.aluvery.ui.AluveryTheme
+import java.math.BigDecimal
 
 class ProductFormActivity : ComponentActivity() {
 
@@ -31,30 +48,51 @@ class ProductFormActivity : ComponentActivity() {
 
         setContent {
             AluveryTheme {
-                Surface {
-                    ProductFormScreen()
-                }
+                Surface { ProductFormScreen() }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun ProductFormScreen() {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(all = 16.dp),
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState())
+            .clickable { keyboardController?.hide() },
         verticalArrangement = Arrangement.spacedBy(16.dp),
         content = {
+            Spacer(modifier = Modifier)
+
             Text(text = "Criando o produto", Modifier.fillMaxWidth(), fontSize = 28.sp)
 
             var url by remember { mutableStateOf("") }
+
+            if (url.isNotBlank()) {
+                AsyncImage(
+                    model = url, contentDescription = null,
+                    Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop,
+                    placeholder = painterResource(id = R.drawable.ic_launcher_background),
+                    error = painterResource(id = R.drawable.ic_launcher_background)
+                )
+            }
+
             TextField(
                 value = url,
                 onValueChange = { url = it },
                 modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
                 label = { Text(text = "URL da imagem") }
             )
 
@@ -63,6 +101,10 @@ fun ProductFormScreen() {
                 value = name,
                 onValueChange = { name = it },
                 modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
                 label = { Text(text = "nome do produto") }
             )
 
@@ -71,6 +113,10 @@ fun ProductFormScreen() {
                 value = price,
                 onValueChange = { price = it },
                 modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Next
+                ),
                 label = { Text(text = "preço do produto") }
             )
 
@@ -81,10 +127,33 @@ fun ProductFormScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(100.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ),
                 label = { Text(text = "descrição do produto") }
             )
 
-            Button(onClick = { /* TODO */ }) { Text(text = "Salvar") }
+            val newPrice = try {
+                BigDecimal(price)
+            } catch (error: NumberFormatException) {
+                BigDecimal.ZERO
+            }
+
+            Button(
+                onClick = {
+                    val product = Product(
+                        name = name,
+                        price = newPrice,
+                        image = url,
+                        description = description
+                    )
+
+                    Log.i("ProductFormActivity", "ProductFormScreen: $product")
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) { Text(text = "Salvar") }
+
+            Spacer(modifier = Modifier)
         }
     )
 }
